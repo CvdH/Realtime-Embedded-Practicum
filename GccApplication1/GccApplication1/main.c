074a2ffe9c36c61078c1110e5d82a5faef78fd2a
Benjistep <benjamin.van.vuuren@hotmail.com>
2017-06-08 15:00:28 +0200

    watchdog working

--- GccApplication1/GccApplication1/main.c
+++ GccApplication1/GccApplication1/main.c
@@ -12,14 +12,14 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include <stdbool.h>
+#include <avr/wdt.h>
 
 #define BAUD 9600
 #define MYUBBR (F_CPU/16/BAUD-1)
 
 void wait(unsigned);
 
-bool received = false;
-uint8_t data = 0;
+#define nRxBuffer 10
 
 //pwm frequentie = clock/(2*prescalar*TOP)
 //16 bit timer phase correct
@@ -29,6 +29,62 @@ uint8_t data = 0;
 // 900   = 0.9ms = 0 graden
 // 1500  = 1.5ms = 90 graden
 // 2100  = 2.1ms = 180 graden
+
+void turnServo(uint16_t degrees)
+{
+	OCR1A = 20000 - ((degrees * 1200 / 180) + 900);
+}
+
+void usart_transmit(uint8_t data[], uint8_t length)
+{
+	for (uint8_t i = 0; i < length; i++)
+	{
+		//wait for empty transmit buffer
+		while (!(UCSR0A & (1 << UDRE0)));
+		UDR0 = data[i];
+	}
+}
+void parseBuffer(uint8_t buffer[], uint8_t length)
+{
+	uint16_t angle = 0;
+	for (uint8_t i = length; i > 0; i--)
+	{
+		uint8_t temp = buffer[length - i] - 48;
+		for (uint8_t j = 0; j < i - 1; j++)
+		{
+			temp *= 10;
+		}
+		angle += temp;
+	}
+	
+	turnServo(angle);
+	uint8_t message[15] = "Turned servo: ";
+	usart_transmit(message, 15);
+	usart_transmit(buffer, length);
+	uint8_t message2[11] = " degrees.\n";
+	usart_transmit(message2, 11);
+}
+
+void addToRxBuffer(uint8_t data)
+{
+	static int rxBufferIndex = 0;
+	static uint8_t rbuffer[nRxBuffer];
+	if (data == '\n')
+	{
+		parseBuffer(rbuffer, rxBufferIndex);
+		rxBufferIndex = 0;
+	}
+	else if (rxBufferIndex < nRxBuffer)
+	{
+		rbuffer[rxBufferIndex++] = data;
+	}
+	
+	if (rxBufferIndex == nRxBuffer)
+	{
+		parseBuffer(rbuffer, rxBufferIndex);
+		rxBufferIndex = 0;
+	}
+}
 
 void timer1_init()
 {
@@ -46,7 +102,7 @@ void timer1_init()
 // called whenever TCNT1 overflows
 ISR(TIMER1_COMPA_vect)
 {
-	PORTB ^= (1 << PB7);  // toggles the led
+	PORTA ^= (1 << PA7);  // toggles the led
 }
 
 void initUSART(uint16_t ubbr)
@@ -60,48 +116,33 @@ void initUSART(uint16_t ubbr)
 	UCSR0C = (1 << USBS0) | (3 << UCSZ00);
 }
 
-void turnServo(uint16_t degrees)
-{	
-	 OCR1A = 20000 - ((degrees * 1200 / 180) + 900);
+void initWatchDog()
+{
+	//disable interrupt
+	cli();
+	wdt_reset();
+	//start timed sequence
+	WDTCSR |= (1 << WDCE) | (1 << WDE);
+	//set prescaler and mode interrupt and system reset
+	WDTCSR = (1 << WDP0) | (1 << WDP3) | (1 << WDE) | (1 << WDIE);
+	sei();
 }
 
 
 int main() {
 	sei();
-	//initUSART(MYUBBR);
+	initUSART(MYUBBR);
+	uint8_t msg[9] = "reboot!\n";
+	usart_transmit(msg, 9);
+	initWatchDog();
 	timer1_init();
-	uint8_t delay = 0;
 	//set pin7 to output
 	DDRB = (1 << PB7);	
 	DDRA = (1 << PA7);
 
 	while (1)
 	{
-		turnServo(90);
-		wait(10);
-		turnServo(120);
-		wait(10);
-/*		if (received)
-		{
-			switch (data)
-			{
-				case '1': delay = 10; break;
-				case '2': delay = 5; break;
-				case '3': delay = 1; break;
-			}
-			received = false;
-		}
-		
-		//PORTB ^= (1 << PB7);
-		//wait(delay);
-		uint16_t count = 0;
-		count = TCNT1H << 8;
-		count += TCNT1L;
-		if (count == 15625)
-		{
-			PORTB ^= (1 << PB7);
-		}
-*/
+
 	}
 }
 
@@ -112,7 +153,14 @@ void wait(unsigned a) {
 
 ISR(USART0_RX_vect)
 {
-	data = UDR0;
-	received = true;
+	wdt_reset();
+	addToRxBuffer(UDR0);
+}
+
+ISR(WDT_vect)
+{
+	turnServo(90);
+	uint8_t message[21] = "Servo to save state\n";
+	usart_transmit(message, 20);
 }
 
